bd75353b29ead5f75ff84b7010f7b45ed0a2f237
arwaldiel <kgergo92@gmail.com>
2025-12-05 16:33:30 +0100

    feat: update hello message

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("Hello World.");
+  res.send("Hello World2.");
 });
 
 app.listen(PORT, () => {
-  console.log(`App fut: http://localhost:${PORT}`);
+  console.log(`App2 fut: http://localhost:${PORT}`);
 });
