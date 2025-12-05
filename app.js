f85b37e321d6035801cf60d458e31a33065f2e58
arwaldiel <kgergo92@gmail.com>
2025-12-05 17:13:42 +0100

    feat: update hello message on feature branch

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("Hello World2.");
+  res.send("Hello World_asd.");
 });
 
 app.listen(PORT, () => {
